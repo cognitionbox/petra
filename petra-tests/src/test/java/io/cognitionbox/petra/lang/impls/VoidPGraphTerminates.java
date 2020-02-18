546d0b32aad3799058f69ae96941b9a5307a9451
Aran Hakki <aran.hakki@gmail.com>
2020-02-18 21:38:07 +0000

    made Person serializable for hazelcast mode

--- petra-tests/src/test/java/io/cognitionbox/petra/lang/impls/VoidPGraphTerminates.java
+++ petra-tests/src/test/java/io/cognitionbox/petra/lang/impls/VoidPGraphTerminates.java
@@ -1,17 +1,32 @@
-/**
- * Copyright 2016-2020 Aran Hakki
+/*
+ ********************************************************************************************************
+ * Copyright © 2016-2020 Cognition Box Ltd. - All Rights Reserved
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of the "Petra" system. "Petra" is owned by:
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Cognition Box Ltd. (10194162)
+ * 9 Grovelands Road,
+ * Palmers Green,
+ * London, N13 4RJ
+ * England.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * "Petra" is Proprietary and Confidential.
+ * Unauthorized copying of "Petra" files, via any medium is strictly prohibited.
+ *
+ * "Petra" can not be copied and/or distributed without the express
+ * permission of Cognition Box Ltd.
+ *
+ * "Petra" includes trade secrets of Cognition Box Ltd.
+ * In order to protect "Petra", You shall not decompile, reverse engineer, decrypt,
+ * extract or disassemble "Petra" or otherwise reduce or attempt to reduce any software
+ * in "Petra" to source code form. You shall ensure, both during and
+ * (if you still have possession of "Petra") after the performance of this Agreement,
+ * that (i) persons who are not bound by a confidentiality agreement consistent with this Agreement
+ * shall not have access to "Petra" and (ii) persons who are so bound are put on written notice that
+ * "Petra" contains trade secrets, owned by and proprietary to Cognition Box Ltd.
+ *
+ * "Petra" is written by Aran Hakki <aran@cognitionbox.io>
+ ********************************************************************************************************
  */
 package io.cognitionbox.petra.lang.impls;
 
@@ -21,10 +36,13 @@ import io.cognitionbox.petra.lang.annotations.Extract;
 import io.cognitionbox.petra.config.ExecMode;
 import io.cognitionbox.petra.lang.PEdge;
 import io.cognitionbox.petra.lang.PGraph;
+import io.cognitionbox.petra.lang.impls.BaseExecutionModesTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+
+import java.io.Serializable;
 
 import static io.cognitionbox.petra.util.Petra.readConsume;
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -39,7 +57,7 @@ public class VoidPGraphTerminates extends BaseExecutionModesTest {
     }
 
     @Extract
-    public static class Person {
+    public static class Person implements Serializable {
         String name;
 
         @Extract
@@ -77,7 +95,7 @@ public class VoidPGraphTerminates extends BaseExecutionModesTest {
     }
 
     // how to terminate with a void someRef…? Need to check this in the code
-    public static class ProcessPerson extends PGraph<@Extract Person, Void> {
+    public static class ProcessPerson extends PGraph<Person, Void> {
         {
             pre(readConsume(Person.class, x->true));
             step(new PrintAge());
@@ -88,9 +106,9 @@ public class VoidPGraphTerminates extends BaseExecutionModesTest {
 
     @Test
     public void terminatesWithVOID() {
-        getGraphComputer().getConfig().setConstructionGuaranteeChecks(false);
+        RGraphComputer.getConfig().setConstructionGuaranteeChecks(false);
         Person person = new Person("Aran",32);
-        Object result = getGraphComputer().computeWithInput(new ProcessPerson(),person);
+        Object result = PGraphComputer.computeWithInput(new ProcessPerson(),person);
         assertThat(result).isInstanceOf(Void.class);
     }
 
