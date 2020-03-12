f5668466482829c826542858c7378a5523774770
Aran Hakki <aran.hakki@gmail.com>
2020-03-12 17:31:16 +0000

    added ref set nullifier

---
+++ petra-core/src/main/java/io/cognitionbox/petra/lang/RefSetNullifier.java
@@ -0,0 +1,45 @@
+package io.cognitionbox.petra.lang;
+
+import io.cognitionbox.petra.core.impl.ReflectUtils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static io.cognitionbox.petra.util.Petra.ref;
+
+public class RefSetNullifier {
+    private Map<Field,Ref> map = new HashMap<>();
+
+
+    // after operation we need to revert back the reference
+
+    public void nullifyAllSettersOfRefsInObject(Object o, boolean trueForNullifyFalseForRevert){
+        Set<Field> fields = ReflectUtils.getAllFieldsAccessibleFromObject(o.getClass())
+                .stream()
+                .filter(f->Ref.class.isAssignableFrom(f.getType()))
+                .collect(Collectors.toSet());
+
+        // need to do this recursively for all other refs
+        for (Field f : fields){
+            try {
+                Ref r = null;
+                if (trueForNullifyFalseForRevert){
+                    r = (Ref) f.get(o);
+                    map.put(f,r); // need to store original ref values against fields, e.g. in a Map
+                    f.set(o,ref(r.get())); // will be set with new ref of same value
+                    // get will return same result but set will set on this new ref rather than original
+                } else {
+                    r = map.get(f);
+                    f.set(o,r); // add original ref back in
+                }
+                // recursive step to
+                nullifyAllSettersOfRefsInObject(r.get(),trueForNullifyFalseForRevert);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
