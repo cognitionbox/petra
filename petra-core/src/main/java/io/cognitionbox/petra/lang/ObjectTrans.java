4dd3081e6016b0de8ebd0ad66d88fdf8572f6f7a
Aran Hakki <aran.hakki@gmail.com>
2020-07-16 22:59:09 +0100

    no need to recurse whole object graph as we just need to copy the first level fields. Need to think about copying whole token value and updating the token with the change value, need to think about the correctness in distributed execution

--- petra-core/src/main/java/io/cognitionbox/petra/lang/ObjectTrans.java
+++ petra-core/src/main/java/io/cognitionbox/petra/lang/ObjectTrans.java
@@ -1,12 +1,12 @@
 package io.cognitionbox.petra.lang;
 
+import io.cognitionbox.petra.core.impl.ObjectCopyerViaSerialization;
 import io.cognitionbox.petra.core.impl.ReflectUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 public class ObjectTrans {
@@ -14,24 +14,39 @@ public class ObjectTrans {
     static class A {
         String x = "x";
         String y = "y";
+        List<Integer> list = new ArrayList<>();
+
+        {
+            list.add(1);
+            list.add(2);
+            list.add(3);
+        }
 
         @Override
         public String toString() {
             return "A{" +
                     "x='" + x + '\'' +
                     ", y='" + y + '\'' +
+                    ", list=" + list +
                     '}';
         }
     }
 
-    private Map<Field,Object> storedValues = new HashMap<>();
+    private ObjectCopyerViaSerialization copyer = new ObjectCopyerViaSerialization();
+    private Map<Field,Object> storedValues = new ConcurrentHashMap<>();
     public ObjectTrans() {}
 
     public void capture(Object object){
-        actionAllFieldsAccessibleFromObjectInstance(object,(f,v)->{
-            // later add support for petra's parallel / distributable atomic reference
-            // we capture/replace the value rather than the reference
-            storedValues.put(f,v);
+        Set<Field> fields = ReflectUtils.getAllFieldsAccessibleFromObject(object.getClass());
+        fields.parallelStream().forEach(f->{
+            try {
+                Object value = f.get(object);
+                // later add support for petra's parallel / distributable atomic reference
+                // we capture/replace the value rather than the reference
+                storedValues.put(f,copyer.copy(value));
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
         });
     }
 
@@ -41,55 +56,33 @@ public class ObjectTrans {
         objectTrans.capture(a);
         System.out.println(a);
         a.x = "hello";
+        a.list.clear();
         System.out.println(a);
         objectTrans.restore(a);
         System.out.println(a);
-
-        A a2 = new A();
-        System.out.println(a2);
-        a2.x = "a";
-        objectTrans.restore(a2);
-        System.out.println(a2);
+//
+//        A a2 = new A();
+//        System.out.println(a2);
+//        a2.x = "a";
+//        objectTrans.restore(a2);
+//        System.out.println(a2);
     }
 
     public void restore(Object object){
-        actionAllFieldsAccessibleFromObjectInstance(object,(f,v)->{
+        Set<Field> fields = ReflectUtils.getAllFieldsAccessibleFromObject(object.getClass());
+        fields.parallelStream().forEach(f->{
             // later add support for petra's parallel / distributable atomic reference
             // we capture/replace the value rather than the reference
-            Object restored = storedValues.get(f);
-            boolean access = f.isAccessible();
             try {
+                boolean access = f.isAccessible();
+                Object restored = storedValues.get(f);
+                // later add support for petra's parallel / distributable atomic reference
+                // we capture/replace the value rather than the reference
                 f.set(object,restored);
+                f.setAccessible(access);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            f.setAccessible(access);
         });
-    }
-
-    static public <T> void actionAllFieldsAccessibleFromObjectInstance(Object value, BiConsumer<Field,Object> fieldConsumer) {
-        if (value==null){
-            return;
-        }
-        if (Boolean.class.isAssignableFrom(value.getClass()) || String.class.isAssignableFrom(value.getClass()) || Integer.class.isAssignableFrom(value.getClass())){
-            return;
-        }
-        Set<Field> fields = ReflectUtils.getAllFieldsAccessibleFromObject(value.getClass());
-        for (Field f : fields){
-            if (!(Boolean.class.isAssignableFrom(value.getClass()) || String.class.isAssignableFrom(value.getClass()) || Integer.class.isAssignableFrom(value.getClass()))){
-                if (!Modifier.isStatic(f.getModifiers())){
-                    try {
-                        boolean isAccessable = f.isAccessible();
-                        f.setAccessible(true);
-                        Object v = f.get(value);
-                        actionAllFieldsAccessibleFromObjectInstance(v,fieldConsumer);
-                        f.setAccessible(isAccessable);
-                        fieldConsumer.accept(f,v);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
     }
 }
