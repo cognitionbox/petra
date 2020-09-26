2f1b739462ebbacf6deb339ff1bda0787b5c7f2d
Aran Hakki <aran.hakki@gmail.com>
2020-09-26 11:59:13 +0100

    fix to remove unused code

--- petra-kotlin/src/main/kotlin/io/cognitionbox/petra/ktln/Petra.kt
+++ petra-kotlin/src/main/kotlin/io/cognitionbox/petra/ktln/Petra.kt
@@ -21,34 +21,14 @@ import kotlin.reflect.KClass
 
 object Petra {
 
-    fun <T:Any> rw(eventClazz: KClass<T>, predicate: (T) -> Boolean): GuardWrite<T> {
-        return GuardWrite(eventClazz.java, predicate)
-    }
-
-    fun <T:Any> rt(eventClazz: KClass<T>, predicate: (T) -> Boolean): GuardReturn<T> {
-        return GuardReturn(eventClazz.java, predicate)
-    }
-
     fun <X> anonymous(p: Guard<X>,
-                         function: (X) -> X,
+                         function: (X)->Unit,
                          vararg qs: Guard<X>): PEdge<X> {
         val pTypeXOR = GuardXOR<X>(OperationType.RETURN)
         for (q in qs) {
             pTypeXOR.addChoice(q)
         }
         return PEdge(p, function, pTypeXOR)
-    }
-
-    fun <A, R> anonymousJ1(a: Guard<A>, function:(List<A>) -> R, r: Guard<R>): PJoin<A, R> {
-        return PJoin(a, function, r)
-    }
-
-    fun <A, B, R> anonymousJ2(a: Guard<A>, b: Guard<B>, function:(List<A>,List<B>) -> R, r: Guard<R>): PJoin2<A, B, R> {
-        return PJoin2(a, b, function, r)
-    }
-
-    fun <A, B, C, R> anonymousJ3(a: Guard<A>, b: Guard<B>, c: Guard<C>, function:(List<A>,List<B>,List<C>) -> R, r: Guard<R>): PJoin3<A, B, C, R> {
-        return PJoin3(a, b, c, function, r)
     }
 
 }
