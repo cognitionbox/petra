6d6c4fc63d7c4ef0846beac9510ffe2be3c2b317
Aran Hakki <aran.hakki@gmail.com>
2021-02-09 20:44:50 +0000

    fix to else step for controlled english rendering

--- petra-tests/src/test/java/io/cognitionbox/petra/lang/impls/steptest/AtoA.java
+++ petra-tests/src/test/java/io/cognitionbox/petra/lang/impls/steptest/AtoA.java
@@ -3,13 +3,12 @@ package io.cognitionbox.petra.lang.impls.steptest;
 import io.cognitionbox.petra.lang.PGraph;
 
 import static io.cognitionbox.petra.util.Petra.par;
-import static io.cognitionbox.petra.util.Petra.seq;
 
-public class AtoAGraph extends PGraph<A> {
+public class AtoA extends PGraph<A> {
     {
       type(A.class);
       pre(a->a.value==1);
       post(a->a.value==222);
-      step(par(),x->x,AtoAEdge.class);
+      step(par(),x->x, AtoAEdge.class);
     }
   }
