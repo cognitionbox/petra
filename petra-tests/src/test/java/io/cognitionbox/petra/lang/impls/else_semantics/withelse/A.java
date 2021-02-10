bcd0481fa33677245188feb3f0d57102f126f992
Aran Hakki <aran.hakki@gmail.com>
2021-02-10 14:22:01 +0000

    resolved thoughts around else semantics and disjoint condition coverage

---
+++ petra-tests/src/test/java/io/cognitionbox/petra/lang/impls/else_semantics/withelse/A.java
@@ -0,0 +1,17 @@
+package io.cognitionbox.petra.lang.impls.else_semantics.withelse;
+
+import java.io.Serializable;
+
+public class A implements Serializable {
+    int value = 0;
+    public A(int value) {
+      this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "value=" + value +
+                '}';
+    }
+}
