1e12b6be5a8077a740c6b0e496acc920bc6931c0
Aran Hakki <aran.hakki@gmail.com>
2021-03-01 20:49:02 +0000

    fix to exception

--- petra-unit/src/test/java/petra/lang/impls/generics/AtoA.java
+++ petra-unit/src/test/java/petra/lang/impls/generics/AtoA.java
@@ -2,13 +2,15 @@ package petra.lang.impls.generics;
 
 import io.cognitionbox.petra.lang.PGraph;
 
+import static io.cognitionbox.petra.util.Petra.choice;
+
 public class AtoA extends PGraph<A> {
     {
       type(A.class);
       pre(a->a.value==0);
       begin();
-      step(BtoB.class);
-      step(CtoC.class);
+      step(choice(),BtoB.class);
+      step(choice(),CtoC.class);
       end();
       post(a->a.value==1);
     }
